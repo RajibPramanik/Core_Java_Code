ee580cb7733095674b6b7c466830b251406930dd
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-01-10 20:49:16 +0530

    Update HelloWorld.java

--- Basic_Part/HelloWorld.java
+++ Basic_Part/HelloWorld.java
@@ -1,15 +1,5 @@
-import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
-        // Read user's name from standard input
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter your name: ");
-        String name = sc.nextLine();
-
-        // Display the entered name
-        System.out.println("Your name is: " + name);
-
-        sc.close();
+        System.out.println("Hello World");
     }
 }
