1eedab905bd250b645da14c11163415886356d85
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-01-11 18:17:24 +0530

    Update AddTwoNumber.java

--- Basic_Part/AddTwoNumber.java
+++ Basic_Part/AddTwoNumber.java
@@ -1,19 +1,10 @@
-import java.util.Scanner;
 public class AddTwoNumber {
     public static void main(String[] args) {
-        // Read numbers from standard input
-        Scanner sc = new Scanner(System.in);
+        int a = 5;
+        int b = 10;
 
-        System.out.print("Enter the first number: ");
-        int num1 = sc.nextInt();
-        System.out.print("Enter the second number: ");
-        int num2 = sc.nextInt();
-
-        // Add the two numbers
-        int sum = num1 + num2;
-        // Print the result
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-
-        sc.close();
+        System.out.println("The first number is: " + a);
+        System.out.println("The second number is: " + b);
+        System.out.println("Addition of two numbers is: " + (a + b));
     }
 }
