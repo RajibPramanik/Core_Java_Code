e074af1f60ae129d78dd09120e6d914e40728022
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-01-14 00:04:38 +0530

    Update Comments.java

--- Basic_Part/Comments.java
+++ Basic_Part/Comments.java
@@ -1,28 +1,13 @@
-/**
- * This class represents a simple calculator.
- * It can perform addition and subtraction.
- */
-public class Calculator {
-
-    /**
-     * This method adds two numbers.
-     * 
-     * @param a the first number
-     * @param b the second number
-     * @return the sum of a and b
-     */
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    /**
-     * This method subtracts two numbers.
-     * 
-     * @param a the first number
-     * @param b the second number
-     * @return the difference of a and b
-     */
-    public int subtract(int a, int b) {
-        return a - b;
+public class Comments{
+    public static void main(String[] args){
+        // Single line comment
+        
+        /* Multi-line 
+        comment */
+        
+        /** Documentation Comment
+         * Git
+         * GitHub
+        */
     }
 }
