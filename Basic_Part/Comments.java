feb54c11ddb96e15ac642eee5324f52037e36a5c
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-01-13 23:59:23 +0530

    Add files via upload

---
+++ Basic_Part/Comments.java
@@ -0,0 +1,28 @@
+/**
+ * This class represents a simple calculator.
+ * It can perform addition and subtraction.
+ */
+public class Calculator {
+
+    /**
+     * This method adds two numbers.
+     * 
+     * @param a the first number
+     * @param b the second number
+     * @return the sum of a and b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * This method subtracts two numbers.
+     * 
+     * @param a the first number
+     * @param b the second number
+     * @return the difference of a and b
+     */
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
