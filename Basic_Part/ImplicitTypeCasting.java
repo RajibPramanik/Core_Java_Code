b2315d5b8d01d64a537522409d385d657819914f
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-01-18 22:35:14 +0530

    Add files via upload

---
+++ Basic_Part/ImplicitTypeCasting.java
@@ -0,0 +1,10 @@
+class ImplicitTypeCasting {
+    public static void main(String[] args) {
+
+        int number = 10;          // int value
+        double x = number;        // implicit typecasting
+
+        System.out.println("Int value is: " + number);
+        System.out.println("After implicit casting to double: " + x);
+    }
+}
