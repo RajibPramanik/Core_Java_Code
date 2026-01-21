17db66b7e4072789e7f73c7df5495f8e1e635a4e
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-01-21 19:53:34 +0530

    Update ArithmeticOperator.java

--- Operators/ArithmeticOperator.java
+++ Operators/ArithmeticOperator.java
@@ -1,7 +1,7 @@
 public class ArithmeticOperator {
     public static void main(String[] args) {
         int a = 10;
-        int b = 3;
+        int b = 5;
 
         System.out.println("Addition: " + (a + b));
         System.out.println("Subtraction: " + (a - b));
