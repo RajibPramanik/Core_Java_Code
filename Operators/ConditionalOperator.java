a765ad90eb82a6da8a6eb8e80609678770da3832
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-01-27 23:32:52 +0530

    Add files via upload

---
+++ Operators/ConditionalOperator.java
@@ -0,0 +1,10 @@
+public class ConditionalOperator {
+    public static void main(String[] args) {
+
+        int number = 8;
+
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println("The number is: " + result);
+    }
+}
