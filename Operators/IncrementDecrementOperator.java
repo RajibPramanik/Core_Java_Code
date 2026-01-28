fae86c3dbce821412b5c27a7e14f3c973fcba78b
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-01-28 18:25:53 +0530

    Add files via upload

---
+++ Operators/IncrementDecrementOperator.java
@@ -0,0 +1,8 @@
+class IncrementDecrementOperator {
+    public static void main(String[] args) {
+        int a = 5;
+
+        System.out.println("Increment value: " + (++a));
+        System.out.println("Decrement value: " + (--a));
+    }
+}
