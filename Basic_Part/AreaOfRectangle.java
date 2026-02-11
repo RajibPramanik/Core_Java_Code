4536f0f120ad5691e705eb8b131e624d86280733
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-02-11 22:38:21 +0530

    Add files via upload

---
+++ Basic_Part/AreaOfRectangle.java
@@ -0,0 +1,13 @@
+public class AreaOfRectangle {
+    public static void main(String[] args){
+        int length = 8;
+        int width = 4;
+        int area;
+
+        area = length * width;
+
+        System.out.println("Length is: "+length);
+        System.out.println("Width is: "+width);
+        System.out.println("Area of the rectangle is: "+area);
+    }
+}
