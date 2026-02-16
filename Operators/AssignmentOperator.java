4bf481d4525247845d88a10d06e1ebbc3da6697b
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-02-17 00:06:25 +0530

    Update AssignmentOperator.java

--- Operators/AssignmentOperator.java
+++ Operators/AssignmentOperator.java
@@ -1,6 +1,25 @@
 public class AssignmentOperator{
     public static void main(String[] args){
-        int x = 5;
-        System.out.println("Assignment Operator is: "+x);
-    }
+		int a = 5;
+		
+		// Arithmatic Operator
+		a += 5;
+		a -= 3;
+		a *= 2;
+		a /= 2;
+		a %= 2;
+		System.out.println(a); // 1
+		
+		// Bitwise operator
+		a &= 2; 
+		a |= 3;
+		a ^= 1;
+		System.out.println(a); // 2
+		
+		// Shift Operator
+		a <<= 1;
+		a >>= 1;
+		a >>>= 1;
+		System.out.println(a); // 1
+	}
 }
