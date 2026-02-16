ebbab31dfc431b5ace8324d144d5ec368d9882a5
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-02-16 08:39:43 +0530

    Add files via upload

---
+++ Basic_Part/LocalVariable.java
@@ -0,0 +1,11 @@
+public class LocalVariable{
+    void show(){
+        // Local Variable
+        int a = 5;
+        System.out.println(a); 
+    }
+    public static void main(String[] args){
+        LocalVariable LV = new LocalVariable();
+        LV.show();
+    }
+}
