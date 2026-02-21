665f9da5d4cf653d06b06d1688058de0e25f1e37
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-02-21 09:38:39 +0530

    Update ConditionalOperator.java

--- Operators/ConditionalOperator.java
+++ Operators/ConditionalOperator.java
@@ -1,10 +1,17 @@
-public class ConditionalOperator {
+class ConditionalOperator {
     public static void main(String[] args) {
 
-        int number = 8;
+        int a = 10; 
+        int b = 20; 
 
-        String result = (number % 2 == 0) ? "Even" : "Odd";
+        int max = (a > b) ? a : b;  
+        System.out.println("Maximum: " + max);  
 
-        System.out.println("The number is: " + result);
+        int result = (a < b) ? (a + b) : (a - b);
+        System.out.println("Result: " + result);  
+ 
+        int min = (a < b) ? ((a < 5) ? a : 5) : ((b < 5) ? b : 5);
+        System.out.println("Minimum using nested ternary: " + min);
+
     }
 }
