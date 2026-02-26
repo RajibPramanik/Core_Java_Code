f63142d6344ffdac154bc138fad576e2589b65c2
Rajib Lochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2026-02-26 11:46:44 +0530

    Update IncrementDecrementOperator.java

--- Operators/IncrementDecrementOperator.java
+++ Operators/IncrementDecrementOperator.java
@@ -1,8 +1,23 @@
-class IncrementDecrementOperator {
-    public static void main(String[] args) {
-        int a = 5;
-
-        System.out.println("Increment value: " + (++a));
-        System.out.println("Decrement value: " + (--a));
-    }
+class IncrementDecrementOperator{
+	public static void main(String[] args){
+		// Pre-increment part
+		int x = 5;
+		int y = ++x;
+		System.out.println(x+" "+y);
+		
+		// Post-increment part
+		int a = 10;
+		int b = a++;
+		System.out.println(a+" "+b);
+		
+		// Pre-decrement part
+		int c = 10;
+		int d = --c;
+		System.out.println(c+" "+d);
+		
+		// Post-decrement part
+		int u = 10;
+		int v = u--;
+		System.out.println(u+" "+v);
+	}
 }
